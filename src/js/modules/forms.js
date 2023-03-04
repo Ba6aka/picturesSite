b45e5cae04ae75dbab18ea6e17da2de16fd912db
Ba6aka <bam6ykba6aka@gmail.com>
2023-03-04 10:49:42 +0200

    Create module forms
    Module with alert user for interactive forms send date

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,57 +1,93 @@
 import { postData } from "../servises/servises";
 
 
-function forms(state){
+const forms = () =>{
     const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input');
-
+          inputs = document.querySelectorAll('input'),
+          upload = document.querySelectorAll('[data-upload]');
+          console.log(document.querySelector('[data-upload]'));
     const message = {
         success:'success',
         loaging: 'loading',
-        fail: 'fail'};
+        fail: 'fail',
+        spinner: 'assets/img/loading.png',
+        failure: 'assets/img/fail.png',
+        ok: 'assets/img/succes.png'};
     
-  
+    const path = {
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
+    };
+ 
 
    function clearInputs(){
         inputs.forEach((input) =>{
             input.value = '';
         });
     }
+   
+    upload.forEach((imageInput) =>{
+        imageInput.addEventListener('input', () =>{
+            let dots;
+            let filesName = imageInput.files[0].name.split('.');
+            filesName[0].length > 5 ? dots = '...' : dots = '.';
+            console.log(filesName[0].length)
+            imageInput.previousElementSibling.textContent = filesName[0].substring(0, 6) + dots + filesName[1];
+        });
+    });
 
     form.forEach((item)=>{
         item.addEventListener('submit', (e) =>{
             e.preventDefault();
-
             let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
-            item.appendChild(statusMessage);
+            item.parentNode.appendChild(statusMessage);
+
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400);
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loaging;
+            statusMessage.appendChild(textMessage);
 
             const formData = new FormData(item);
-            if (item.getAttribute('data-calc') === "end"){
-                for (let key in state){
-                    formData.append(key, state[key]);
-                }
-            }
+            let api;
+            item.closest('.popup-consultation') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
+            console.log(api);
            
+          
 
-            postData('assets/server.php', formData, '.status', message.loaging)
+            postData(api, formData)
             .then(res =>{
                 console.log(res);
-                document.querySelector('.status').textContent = message.success;
-                setTimeout(() => {
-                    document.querySelectorAll('[data-modal]').forEach((item) =>{
-                        item.style.display = 'none';
-                        document.body.style.overflow = '';
-                    });
-                }, 5000);
+                statusImg.setAttribute('src', message.ok);
+                textMessage.textContent = message.success;
+               
             })
             .catch(() =>{
-                document.querySelector('.status').textContent = message.fail;
+               textMessage.textContent = message.fail;
+               statusMessage.setAttribute('src', message.failure);
             })
             .finally(()=>{
                 clearInputs();
                 setTimeout(() => {
+                    item.style.display = 'block';
+                    item.classList.remove('fadeOutUp');
+                    item.classList.add('fadeInUp');
                     statusMessage.remove();
+                    textMessage.remove();
+                    upload.forEach((item) =>{
+                        console.log(item);
+                        console.log('something')
+                        item.previousElementSibling.textContent = 'file not selected';
+                    });
                 }, 5000);
             }
                
