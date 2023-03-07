32bd2357489087391bb39422ad3421ee7b69c8c2
Ba6aka <bam6ykba6aka@gmail.com>
2023-03-07 16:11:30 +0200

    add event listener
    listener for chek language in text area and input

--- src/js/modules/checkLanguageInputs.js
+++ src/js/modules/checkLanguageInputs.js
@@ -1,5 +1,13 @@
 const checkLanguageInputs = (selector) =>{
+    const inputs = document.querySelectorAll(selector);
 
+    inputs.forEach((item) =>{
+        item.addEventListener('keypress', (e) =>{
+            if (e.key.match(/[^a-z 0-9]/ig)){
+                e.preventDefault();
+            }
+        });
+    });
 };
 
 export {checkLanguageInputs};
